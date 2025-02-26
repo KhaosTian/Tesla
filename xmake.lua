6ca1430bef4b4f244c150c7271bf7f75d5afc223
tzhh <87858978+KhaosTian@users.noreply.github.com>
2025-02-26 23:57:21 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,18 +1,14 @@
-
 set_project("Tesla")
 set_version("0.1.0")
 
---编译设置
 set_arch("x64")
 set_plat("windows")
 set_toolchains("msvc")
 set_languages("c++20")
 
---规则
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 
---包依赖
 add_requires("tbb", "glm", "assimp", "stb")
 
 target("Tesla")
